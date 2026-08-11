0358cd70e076cf8f9507f1b9c970d819b4c615c7
Peehu1308 <peehumishra1308@gmail.com>
2026-08-11 23:41:53 +0530

    Shortid

--- controllers/url.js
+++ controllers/url.js
@@ -3,12 +3,16 @@ const URL=require('../models/url')
 
 async function handleGenerateNewShortURL(req,res){
     const body=req.body;
-    if(!body.url)return res.status(400).json({error:'url is required'});
-    const shortid=nanoid(8);
+    if(!body.url) return res.status(400).json({error:'url is required'});
+    const shortId=nanoid(8);
     await URL.create({
-        shortid:shortid,
-        redirectURL:body.url,
-        visitHistory:[],
+        shortId: shortId,
+        redirectURL: body.url,
+        visitHistory: [],
     });
     return res.json({id:shortId});
+}
+
+module.exports={
+    handleGenerateNewShortURL
 }
